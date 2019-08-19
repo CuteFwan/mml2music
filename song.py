b8eced98cf86aca0f9390bc406bdc73b46346308
CuteFwan <flandre.scarlet66@gmail.com>
2019-08-18 20:44:22 -0400

    note and track class

---
+++ song.py
@@ -0,0 +1,25 @@
+class Note:
+    __slots__ = (position, frequency, length, volume)
+
+    def __init__(self, position : float, frequency : float, length : float, volume : float):
+        self.position = position
+        self.frequency = frequency
+        self.length = length
+        self.volume = volume
+
+class Track:
+
+    def __init__(self):
+        self.notes = list()
+        self.position = 0
+
+    def add_note(self, note : Note):
+        self.notes.append(note)
+        self.position += note.length
+
+    def extend_last(self, length : float):
+        self.notes[-1].length += length
+        self.position += length
+
+    def rest(self, length : float):
+        self.position += length
