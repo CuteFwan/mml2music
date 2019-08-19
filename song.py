8640628abb1099ce40648797cea33a096899880b
CuteFwan <flandre.scarlet66@gmail.com>
2019-08-18 21:00:15 -0400

    PEP8 variable annotations

--- song.py
+++ song.py
@@ -1,7 +1,7 @@
 class Note:
     __slots__ = ('position', 'frequency', 'length', 'volume')
 
-    def __init__(self, position : float, frequency : float, length : float, volume : float):
+    def __init__(self, position: float, frequency: float, length: float, volume: float):
         self.position = position
         self.frequency = frequency
         self.length = length
@@ -13,13 +13,13 @@ class Track:
         self.notes = list()
         self.position = 0
 
-    def add_note(self, note : Note):
+    def add_note(self, note: Note):
         self.notes.append(note)
         self.position += note.length
 
-    def extend_last(self, length : float):
+    def extend_last(self, length: float):
         self.notes[-1].length += length
         self.position += length
 
-    def rest(self, length : float):
+    def rest(self, length: float):
         self.position += length
