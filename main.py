1a1f3dc670313db68e782e3f07c5081022a5391b
CuteFwan <flandre.scarlet66@gmail.com>
2019-08-20 22:11:50 -0400

    removed redundant imports

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from pippi import dsp
-from mml2music.mmlparser import MMLParser
-from mml2music.writer import Writer
+import mml2music
 
 with open('input.mml', 'r') as file:
     mml = file.read().lower()
 
-parser = MMLParser()
+parser = mml2music.MMLParser()
 
 track = parser.get_notes(mml)
 
@@ -14,7 +13,7 @@ print(f'Parsed {len(track.notes)} notes.\nTotal length: {track.position}')
 
 out = dsp.buffer(channels=1)
 
-writer = Writer('flute', out)
+writer = mml2music.Writer('flute', out)
 
 writer.compose(track)
 
