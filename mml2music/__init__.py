7bc61b9de93822454ec26135d8e53419e54e7949
CuteFwan <flandre.scarlet66@gmail.com>
2019-11-21 19:28:33 -0500

    Add name, copyright, and version information

--- mml2music/__init__.py
+++ mml2music/__init__.py
@@ -1,3 +1,9 @@
+__title__ = "mml2music"
+__author__ = "CuteFwan"
+__license__ = "MIT"
+__copyright__ = "Copyright CuteFwan"
+__version__ = "0.0.1"
+
 from .song import Note, Track
 from .writer import Writer
 from .mmlparser import MMLParser
